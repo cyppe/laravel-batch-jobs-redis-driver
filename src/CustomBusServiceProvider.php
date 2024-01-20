886d194673a7424764db39c7f9d80376bb11ee54
cyppe <thomas.andersson@gmail.com>
2024-01-20 14:37:13 +0100

    Fix binding of singleton only when queue.batching.database condig is set to 'redis'

--- src/CustomBusServiceProvider.php
+++ src/CustomBusServiceProvider.php
@@ -11,16 +11,17 @@ class CustomBusServiceProvider extends BusServiceProvider
     protected function registerBatchServices()
     {
         $driver = config('queue.batching.database');
-        if ($driver !== 'redis') {
-            return;
-        }
 
-        $this->app->singleton(
-            BatchRepository::class, function ($app) {
-            $factory = $app->make(\Illuminate\Bus\BatchFactory::class);
-            return new RedisBatchRepository($factory);
+        // Only bind the RedisBatchRepository when 'redis' is the selected driver
+        if ($driver === 'redis') {
+            $this->app->singleton(
+                BatchRepository::class, function ($app) {
+                $factory = $app->make(\Illuminate\Bus\BatchFactory::class);
+                return new RedisBatchRepository($factory);
+            }
+            );
         }
-        );
+        // If 'redis' is not the driver, do not override the default binding
     }
 
 }
